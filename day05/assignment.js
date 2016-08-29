b38974db01b55771417db75ebde2ea5c7f8b12c3
pratimasakinala <pratimasakinala13@gmail.com>
2016-08-29 13:41:08 -0700

    day06

--- day05/assignment.js
+++ day05/assignment.js
@@ -1,13 +1,12 @@
 var btn = document.getElementById('btn');
 var input1 = document.getElementById('input1');
-var input2 = document.getElementById('input2').value;
+var input2 = document.getElementById('input2');
 var box = document.getElementById('box');
 
 function btnClick(){
-  input1 = 'Hello javascript!';
-  document.getElementById('input1').value = input1;
-  alert(input1);
-  document.getElementById('box').innerHTML = input2;
+  input1.value = 'Hello javascript!';
+  alert(input1.value);
+  box.innerHTML = input2.value;// box.textContent = input2.value
 }
 
 btn.onclick = btnClick; //no paranthesis means the function doesnt get called untill button is clicked
